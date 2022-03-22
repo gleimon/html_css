56b347ad066ba03419b3c972169d010aecca6f32
gleimon <93679771+gleimon@users.noreply.github.com>
2022-03-21 22:56:51 -0300

    Update script.js

--- site_gleimon/script.js
+++ site_gleimon/script.js
@@ -5,7 +5,7 @@ function valida(){
 
     const user = email.split("@")[0] //para verificar os caracteres até o "@" (user)
     const restante_do_endereco_email = email.split("@")[1] // para verificar os caracteres após o "@" (dominio.com...)
-    const dominio = restante_do_endereco_email.split(".com")[0] // para verificar os caracteres entre o "@" e o ".com" (dominio)
+    const dominio = restante_do_endereco_email.split(".")[0] // para verificar os caracteres entre o "@" e o ".com ou .br" (dependendo do tipo de email) - (dominio)
     
     const tamanho_user = user.length //numero de caracteres do user 
     const tamanho_dominio = dominio.length //numero de caracteres do dominio
